2bf5174254a1774a163a5c0322eab58089edd9ca
nathayoung <nathanayoung@msn.com>
2020-01-24 19:37:04 -0500

    updating update_test.bat

--- update_test.bat
+++ update_test.bat
@@ -1,29 +1,39 @@
-REM git config --global user.name "NCDataDashboard"
+python -m pip install --upgrade pip --user
+pip install jupyter --upgrade
+pip install jupyter lab --upgrade
+pip install pandas --upgrade
+pip install tensorflow --upgrade
+pip install ZipFile --upgrade
+pip install Requests --upgrade
+pip install watermark --upgrade
+pip install xlrd --upgrade
+
+git config --global user.name "nathayoung"
 
 cd Land
-call land_test.bat
+call land.bat
 cd..
 
 cd Labor
-call labor_test.bat
+call labor.bat
 cd..
 
 cd Earnings
-call earnings_test.bat
+call earnings.bat
 cd..
 
 REM cd Demographics
-REM start Demographics_test.bat
+REM start Demographics.bat
 REM cd..
 
 REM cd Health
-REM start health_test.bat
+REM start health.bat
 REM cd..
 
 REM cd Natural Products
-REM start natproducts_test.bat
+REM start natproducts.bat
 REM cd..
 
-REM git status
+git status
 
 exit

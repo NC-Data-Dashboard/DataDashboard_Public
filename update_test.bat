dd502a9d69bc13d320191a548b2f3a0c6840f156
Nathan Young <46688736+nathayoung@users.noreply.github.com>
2020-01-26 15:08:33 -0500

    Update update_test.bat

--- update_test.bat
+++ update_test.bat
@@ -1,11 +1,9 @@
 python -m pip install --upgrade pip --user
 python -m pip install jupyter --upgrade --user
-python -m pip install jupyter lab --upgrade --user
 python -m pip install pandas --upgrade --user
-python -m pip install tensorflow --upgrade --user
 python -m pip install Requests --upgrade --user
 python -m pip install watermark --upgrade --user
-python -m pip install xlrd --upgrade -user
+python -m pip install xlrd --upgrade --user
 
 REM git config --global user.name "NCDataDashboard"
 

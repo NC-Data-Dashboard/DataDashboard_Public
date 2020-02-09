41aa389cc952113ca9bbc8f9b42d5b6db5309f73
NCDataDashboard <nyoung@wcu.edu>
2020-02-09 03:23:18 -0500

    Labor Update Sun 02/09/2020

--- Labor/Scripts/CAINC6N_NC.py
+++ Labor/Scripts/CAINC6N_NC.py
@@ -143,7 +143,7 @@ for i in column_list:
 
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Compensation_of_Employees_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Compensation_of_Employees_BACKUP')
 
 
 # In[ ]:
@@ -270,7 +270,7 @@ for i in column_list:
 
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Wages_and_Salaries_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Wages_and_Salaries_BACKUP')
 
 
 # In[ ]:
@@ -397,7 +397,7 @@ for i in column_list:
 
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Health_Care_and_Social_Assistance_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Health_Care_and_Social_Assistance_BACKUP')
 
 
 # In[ ]:
@@ -524,7 +524,7 @@ for i in column_list:
 
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Information_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Information_BACKUP')
 
 
 # In[ ]:
@@ -627,7 +627,7 @@ for i in column_list:
     df_management.loc[df_management[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Management_of_Companies_and_Enterprises_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Management_of_Companies_and_Enterprises_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Management_of_Companies_and_Enterprises','STG_BEA_CA6N_Management_of_Companies_and_Enterprises_BACKUP';''')
@@ -718,7 +718,7 @@ for i in column_list:
     df_manufacturing.loc[df_manufacturing[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Manufacturing_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Manufacturing_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Manufacturing','STG_BEA_CA6N_Manufacturing_BACKUP';''')
@@ -809,7 +809,7 @@ for i in column_list:
     df_mining.loc[df_mining[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Mining_Quarrying_and_Oil_and_Gas_Extraction_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Mining_Quarrying_and_Oil_and_Gas_Extraction_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Mining_Quarrying_and_Oil_and_Gas_Extraction','STG_BEA_CA6N_Mining_Quarrying_and_Oil_and_Gas_Extraction_BACKUP';''')
@@ -900,7 +900,7 @@ for i in column_list:
     df_services.loc[df_services[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Other_Services_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Other_Services_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Other_Services','STG_BEA_CA6N_Other_Services_BACKUP';''')
@@ -992,7 +992,7 @@ for i in column_list:
     df_professional.loc[df_professional[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Professional_Scientific_and_Technical_Services_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Professional_Scientific_and_Technical_Services_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Professional_Scientific_and_Technical_Services','STG_BEA_CA6N_Professional_Scientific_and_Technical_Services_BACKUP';''')
@@ -1084,7 +1084,7 @@ for i in column_list:
     df_realestate.loc[df_realestate[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Real_Estate_and_Rental_and_Leasing_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Real_Estate_and_Rental_and_Leasing_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Real_Estate_and_Rental_and_Leasing','STG_BEA_CA6N_Real_Estate_and_Rental_and_Leasing_BACKUP';''')
@@ -1176,7 +1176,7 @@ for i in column_list:
     df_retail.loc[df_retail[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Retail_Trade_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Retail_Trade_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Retail_Trade','STG_BEA_CA6N_Retail_Trade_BACKUP';''')
@@ -1268,7 +1268,7 @@ for i in column_list:
     df_transportation.loc[df_transportation[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Transportation_and_Warehousing_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Transportation_and_Warehousing_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Transportation_and_Warehousing','STG_BEA_CA6N_Transportation_and_Warehousing_BACKUP';''')
@@ -1360,7 +1360,7 @@ for i in column_list:
     df_utilities.loc[df_utilities[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Utilities_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Utilities_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Utilities','STG_BEA_CA6N_Utilities_BACKUP';''')
@@ -1452,7 +1452,7 @@ for i in column_list:
     df_wholesale.loc[df_wholesale[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Wholesale_Trade_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Wholesale_Trade_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Wholesale_Trade','STG_BEA_CA6N_Wholesale_Trade_BACKUP';''')
@@ -1547,7 +1547,7 @@ for i in column_list:
     df_pension.loc[df_pension[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Employer_Contributions_for_Employee_Pension_and_Insurance_Funds_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Employer_Contributions_for_Employee_Pension_and_Insurance_Funds_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Employer_Contributions_for_Employee_Pension_and_Insurance_Funds','STG_BEA_CA6N_Employer_Contributions_for_Employee_Pension_and_Insurance_Funds_BACKUP';''')
@@ -1639,7 +1639,7 @@ for i in column_list:
     df_social.loc[df_social[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Employer_Contributions_for_Government_Social_Insurance_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Employer_Contributions_for_Government_Social_Insurance_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Employer_Contributions_for_Government_Social_Insurance','STG_BEA_CA6N_Employer_Contributions_for_Government_Social_Insurance_BACKUP';''')
@@ -1731,7 +1731,7 @@ for i in column_list:
     df_gov.loc[df_gov[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Government_and_Government_Enterprises_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Government_and_Government_Enterprises_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Government_and_Government_Enterprises','STG_BEA_CA6N_Government_and_Government_Enterprises_BACKUP';''')
@@ -1823,7 +1823,7 @@ for i in column_list:
     df_private.loc[df_private[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Private_Nonfarm_Compensation_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Private_Nonfarm_Compensation_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Private_Nonfarm_Compensation','STG_BEA_CA6N_Private_Nonfarm_Compensation_BACKUP';''')
@@ -1915,7 +1915,7 @@ for i in column_list:
     df_farm.loc[df_farm[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Farm_Compensation_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Farm_Compensation_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Farm_Compensation','STG_BEA_CA6N_Farm_Compensation_BACKUP';''')
@@ -2007,7 +2007,7 @@ for i in column_list:
     df_nonfarm.loc[df_nonfarm[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Nonfarm_Compensation_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Nonfarm_Compensation_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Nonfarm_Compensation','STG_BEA_CA6N_Nonfarm_Compensation_BACKUP';''')
@@ -2099,7 +2099,7 @@ for i in column_list:
     df_supplement.loc[df_supplement[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Supplements_to_Wages_and_Salaries_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Supplements_to_Wages_and_Salaries_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Supplements_to_Wages_and_Salaries','STG_BEA_CA6N_Supplements_to_Wages_and_Salaries_BACKUP';''')
@@ -2194,7 +2194,7 @@ for i in column_list:
     df_comp.loc[df_comp[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Average_Compensation_Per_Job_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Average_Compensation_Per_Job_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Average_Compensation_Per_Job','STG_BEA_CA6N_Average_Compensation_Per_Job_BACKUP';''')
@@ -2286,7 +2286,7 @@ for i in column_list:
     df_food.loc[df_food[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Accommodation_and_Food_Services_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Accommodation_and_Food_Services_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Accommodation_and_Food_Services','STG_BEA_CA6N_Accommodation_and_Food_Services_BACKUP';''')
@@ -2378,7 +2378,7 @@ for i in column_list:
     df_admin.loc[df_admin[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services','STG_BEA_CA6N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services_BACKUP';''')
@@ -2470,7 +2470,7 @@ for i in column_list:
     df_arts.loc[df_arts[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Arts_Entertainment_and_Recreation_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Arts_Entertainment_and_Recreation_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Arts_Entertainment_and_Recreation','STG_BEA_CA6N_Arts_Entertainment_and_Recreation_BACKUP';''')
@@ -2562,7 +2562,7 @@ for i in column_list:
     df_construction.loc[df_construction[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Construction_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Construction_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Construction','STG_BEA_CA6N_Construction_BACKUP';''')
@@ -2654,7 +2654,7 @@ for i in column_list:
     df_eduserv.loc[df_eduserv[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Educational_Services_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Educational_Services_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Educational_Services','STG_BEA_CA6N_Educational_Services_BACKUP';''')
@@ -2746,7 +2746,7 @@ for i in column_list:
     df_finance.loc[df_finance[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Finance_and_Insurance_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Finance_and_Insurance_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Finance_and_Insurance','STG_BEA_CA6N_Finance_and_Insurance_BACKUP';''')
@@ -2838,7 +2838,7 @@ for i in column_list:
     df_forestry.loc[df_forestry[i].isnull(),i]=0
 
 # Drop old backup table
-#c.execute('drop table STG_BEA_CA6N_Forestry_Fishing_and_Related_Activities_BACKUP')
+c.execute('drop table STG_BEA_CA6N_Forestry_Fishing_and_Related_Activities_BACKUP')
 
 # Create new backup
 c.execute('''sp_rename 'dbo.STG_BEA_CA6N_Forestry_Fishing_and_Related_Activities','STG_BEA_CA6N_Forestry_Fishing_and_Related_Activities_BACKUP';''')

10aa59b644743fb93a0de91c41b3133b438e415b
NCDataDashboard <nyoung@wcu.edu>
2020-02-13 10:12:46 -0500

    Land Update Thu 02/13/2020

--- Land/Scripts/GeoFRED_All_Transactions_House_Price_Index.py
+++ Land/Scripts/GeoFRED_All_Transactions_House_Price_Index.py
@@ -102,7 +102,7 @@ c = con.cursor()
 
 
 #Drop old backup table
-#c.execute('drop table STG_FRED_All_Transactions_House_Price_Index_by_County_BACKUP')
+c.execute('drop table STG_FRED_All_Transactions_House_Price_Index_by_County_BACKUP')
 
 
 # In[ ]:

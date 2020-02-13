48d3f3387f3ba22232b4d04f946eb68ea1ed10eb
NCDataDashboard <nyoung@wcu.edu>
2020-02-13 10:17:16 -0500

    Demographics Update Thu 02/13/2020

--- Land/Scripts/GeoFRED_All_Transactions_House_Price_Index.py
+++ Land/Scripts/GeoFRED_All_Transactions_House_Price_Index.py
@@ -34,7 +34,7 @@ df_backup.to_csv('./Backups/STG_FRED_All_Transactions_House_Price_Index_by_Count
 
 
 # Getting and reading new data 
-df = pd.read_excel("https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90&lat=40&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=942&attributes=Not+Seasonally+Adjusted%2C+Annual%2C+Index+2000%3D100&aggregationFrequency=Annual&aggregationType=Average&transformation=lin&date=2018-01-01&type=xls&startDate=1975-01-01&endDate=2018-01-01&mapWidth=2000&mapHeight=1262&hideLegend=false", skiprows=1)
+df = pd.read_excel("https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90&lat=40&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=942&attributes=Not+Seasonally+Adjusted%2C+Annual%2C+Index+2000%3D100&aggregationFrequency=Annual&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1975-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1262&hideLegend=false", skiprows=1)
 df.head(2)
 
 
